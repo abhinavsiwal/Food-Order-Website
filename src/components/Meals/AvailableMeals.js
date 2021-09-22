9e4818546932950a7a7a637f8dbb8226360da9b8
abhinavsiwal <abhinavsiwal@gmail.com>
2021-09-22 17:01:46 +0530

    Initial Commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,29 @@
+
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+import meals from './MealData'
+
+const AvailableMeals = () => {
+ 
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
